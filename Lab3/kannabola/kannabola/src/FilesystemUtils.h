50802da6c4f55d5545f5aefedd51df8c68d50555
Smi1le <tima55510@gmail.com>
2016-11-08 21:07:38 +0300

    Добавлена первая часть третьей лабы: травка)

---
+++ Lab3/kannabola/kannabola/src/FilesystemUtils.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Utils.h"
+#include <string>
+#include <boost/filesystem/path.hpp>
+#include <boost/system/error_code.hpp>
+
+class CFilesystemUtils
+{
+public:
+    CFilesystemUtils() = delete;
+
+    static boost::filesystem::path GetResourceAbspath(const boost::filesystem::path &path);
+    static std::string LoadFileAsString(const boost::filesystem::path &path);
+    static SDLSurfacePtr LoadFileImage(const boost::filesystem::path &path);
+    static TTFFontPtr LoadFixedSizeFont(const boost::filesystem::path &path, int pointSize);
+};
