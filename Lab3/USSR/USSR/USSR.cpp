9e847d4fee315ad412c8387130a3d57fa2c4e8eb
Smi1le <tima55510@gmail.com>
2016-11-20 19:03:05 +0300

    Добавлена флаг СССР(Вариант 8) и Кольцо(Вариант 4)

---
+++ Lab3/USSR/USSR/USSR.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "WindowClient.h"
+#include <SDL2/SDL.h>
+
+// Выключаем макроподмену main на SDL_main,
+// т.к. приложение собирается c SUBSYSTEM:CONSOLE
+#ifdef _WIN32
+#undef main
+#endif
+
+
+int main(int, char *[])
+{
+	try
+	{
+		CWindow window;
+		window.Show("USSR flag", { 800, 600 });
+		CWindowClient client(window);
+		window.DoMainLoop();
+	}
+	catch (const std::exception &ex)
+	{
+		const char *title = "Fatal Error";
+		const char *message = ex.what();
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, message, nullptr);
+	}
+
+	return 0;
+}
+
---
+++ Lab3/USSR/USSR/libchapter3.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "src/libchapter3_private.h"
+#include "src/Utils.h"
+#include "src/FilesystemUtils.h"
+#include "src/Window.h"
+#include "src/AbstractWindowClient.h"
+#include "src/Camera.h"
+#include "src/Lights.h"
+
+#include "src/ProgramUniform.h"
+#include "src/VertexAttribute.h"
+#include "src/ShaderProgram.h"
+#include "src/ProgramInfo.h"
